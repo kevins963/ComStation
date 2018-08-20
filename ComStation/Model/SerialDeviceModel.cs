0cd7224d364573186da626e9fd9591bf7d737c9f
kevins963 <kevins963@hotmail.com>
2018-08-20 14:49:39 -0700

    Adding default serial device model

---
+++ ComStation/Model/SerialDeviceModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.SerialCommunication;
+
+namespace ComStation.Model
+{
+    public class SerialDeviceModel : Template10.Mvvm.BindableBase
+    {
+        SerialDevice _serialDevice;
+
+        private void OnSerialDeviceErrorReceived(SerialDevice sender, ErrorReceivedEventArgs args)
+        {
+            // TODO what should happen during error
+            throw new NotImplementedException();
+        }
+
+        private void OnSerialDevicePinChanged(SerialDevice sender, PinChangedEventArgs args)
+        {
+            // TODO what should happen during pin changed
+            throw new NotImplementedException();
+        }
+
+        public SerialDeviceModel(SerialDevice serialDevice)
+        {
+            _serialDevice = serialDevice;
+
+            _serialDevice.ErrorReceived += OnSerialDeviceErrorReceived;
+            _serialDevice.PinChanged += OnSerialDevicePinChanged;
+        }
+
+    }
+}
